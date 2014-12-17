9477eda9735c8c434679ada547acae38833e0e53
tophat8855 <hrfarley@gmail.com>
2014-12-17 15:29:19 -0800

    add edit view and do bonus

--- app/models/job_title.rb
+++ app/models/job_title.rb
@@ -1,3 +1,6 @@
 class JobTitle < ActiveRecord::Base
   has_many :employments
+  has_many :organizations, through: :employments
+  has_many :people, through: :employments
+  has_many :location, through: :employments
 end
